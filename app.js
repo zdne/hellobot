04a47ada396ede224e2a02413aba9cabcdfc4ec0
Z <z@yt.local>
2016-05-22 16:43:18 +0200

    Fix access token ENV VAR

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.post('/webhook', function (req, res) {
 function sendMessage(recipientId, message) {
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        qs: {access_token: process.env.FB_PAGES_ACCESS_TOKEN},
         method: 'POST',
         json: {
             recipient: {id: recipientId},
