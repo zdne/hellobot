6418cdf94bd6316ee98538ac24b3bc1d677a05ca
Z <zdenek@me.com>
2016-05-29 08:54:25 +0200

    Fix user profile

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const Config = require('./lib/config')
 const messenger = require('./lib/messenger')
 
-const UserProfile = require('./lib/schema')
+const UserProfile = require('./lib/schema').UserProfile
 
 var app = require('express')()
 
@@ -64,13 +64,12 @@ app.post('/webhook', (req, res) => {
 function handleMessage(event) {
     messenger.getUserProfile(event.sender.id, (err, profile) => {
         
-        user = new UserProfile(profile)
-        
         if (err) {
             console.log(`error retrieving user profile: ${err}`)
             return
         }
-                    
+        
+        user = new UserProfile(profile)
         messenger.sendMessage(event.sender.id, { text: `Hello ${user.first_name}!` }, (err) => {
             if (err)
                 console.log(`error sending message: ${err}`)
